2c1339312848efd9bc59b06be84149cae29cff67
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-11-14 01:02:57 +0200

    add code for task generic

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,13 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+class Component <T>  {
+  constructor(public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component <{title:string}> {
   pageInfo () {
     console.log(this.props.title);
   }
